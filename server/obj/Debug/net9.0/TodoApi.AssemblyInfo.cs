99e4586f16bd261bec448ae9154501a97a199947
m0556722858 <m0556722858@gmail.com>
2025-03-18 16:52:02 +0200

    ff

--- server/obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce8563bbab1b2407640596494b7c7025fb46fadb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3d5485079be34bdd8eaae7a1707457ca83f5b6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
